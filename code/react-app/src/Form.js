4196c4c073c1607913311e89f1c65e4da1cc6a1f
Dimitri Law <dimitrilaw112@gmail.com>
2022-02-12 15:43:05 -0500

    updated changes

---
+++ code/react-app/src/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Forms.css";
+
+function Form() {
+  return (
+  
+  
+  <div className='Forms'>
+<h1> hello paul</h1>
+  </div>
+  )
+}
+
+export default Form;
