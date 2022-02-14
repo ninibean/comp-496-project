aafd9121b841a238fa88bda6ec72addd780f82bb
Niara Patterson <nspatterson@aggies.ncat.edu>
2022-02-14 17:59:19 -0500

    Attempted to add a grade dropdown menu, GPA now prints out in seperate div instead of alert

--- code/react-app/src/App.js
+++ code/react-app/src/App.js
@@ -5,23 +5,54 @@ import './App.css';
 
 class App extends React.Component {
     addClass() {
+        // Creates the user input boxes
+
         const c = document.createElement("div");
         const cgbox = document.createElement("textarea");
         const chbox = document.createElement("textarea");
+        // const cGradeDropDown = document.createElement("select");
+        // cGradeDropDown.id = "gradeSelect"
         c.appendChild(cgbox);
         c.appendChild(chbox);
+        // c.appendChild(cGradeDropDown);
         document.getElementById("classInfo").appendChild(c);
     }
     
     manualGPA() {
-        var creditEarned=0.0;
-        var totalHours=0.0;
+        // Calculates the GPA and outputs the result
+
+        // TODO: turn dropdown menu into a component
+        var creditEarned = 0.0;
+        var totalHours = 0.0;
         var grade;
         var hours;
+        var finalGPA = 0.0;
+        // var gradeLetters = ["A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "F"]
+        // var gradeNumbers = [4.0, 3.7, 3.3, 3.0, 2.7, 2.3, 2.0, 1.7, 1.3, 1.0, 0.0]
+        // var courseName;
+
+        const GPACalculation = document.createElement("div");
+        document.getElementById("calculatedGPA").appendChild(GPACalculation);
+        var staticTextTag = document.createElement("p");
+        var GPATextTag = document.createElement("p");
+        var staticText = document.createTextNode("Overall GPA: ");
+        staticTextTag.appendChild(staticText);
+
     
         const container = document.getElementById("classInfo");
-        const courses = container.getElementsByTagName("div");
+        const courses = container.getElementsByTagName("div");;
+        const GPAContainer = document.getElementById("calculatedGPA");
+
+        // for (let i = 0; i < gradeLetters.length; i++) {
+        //     var option = document.createElement("option");
+        //     option.value = gradeNumbers[i];
+        //     option.text = gradeLetters[i];
+        //     document.createElement("select").appendChild(option);
+            
+        // }
+
         for(let i = 0; i < courses.length; i++) {
+            // courseName = courses[i].getElementsByTagName("textarea")[0].value;
             grade = courses[i].getElementsByTagName("textarea")[0].value;
             hours = courses[i].getElementsByTagName("textarea")[1].value;
             if (/\d/.test(grade) && /\d/.test(hours)) {
@@ -33,21 +64,26 @@ class App extends React.Component {
                 break;
             }
         }
-        alert(creditEarned / totalHours);
+        // alert(Math.round(100 * (creditEarned / totalHours) / 100));
+        // alert(creditEarned / totalHours);
+        finalGPA = creditEarned / totalHours;
+        var finalGPAString = finalGPA.toString();
+        var finalGPAText = document.createTextNode(finalGPAString)
+        GPATextTag.appendChild(finalGPAText);
+        GPAContainer.appendChild(staticTextTag);
+        GPAContainer.appendChild(GPATextTag);
+
     }
 
     render () {
         return (
             <div className="App">
-                {/* <script type="text/javascript" src="code/react-app/public/js/script.js"></script> */}
                 <p> Please click the "Add Class" button to modify data. Enter the course grade to the left, and the course weight on the right.</p>
                 <p>Grade/Hours</p>
                 <div id="classInfo"></div>
-                {/* <input type="button" onClick="addClass()" value="Add Class"></input> */}
                 <button onClick={this.addClass}> Add Class </button>
                 <button onClick={this.manualGPA}> Calculate GPA </button>
-                {/* <input type="button" onClick="manualGPA()" value="Calculate"></input> */}
-                
+                <div id="calculatedGPA"></div>                
             </div>
         );
     }
