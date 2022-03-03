223069242f68ba93bc8b31020fd4e6295441a900
akinlabimh <akinlabimh@gmail.com>
2022-03-03 11:47:39 -0500

    page output

--- code/react-app/src/login/features/GpaFetch.js
+++ code/react-app/src/login/features/GpaFetch.js
@@ -7,19 +7,27 @@ const GpaFetch = () => {
   const samplestudentgpa = json["studentsInfo"][31]["gpa"];
   var flavortext = "";
   if (samplestudentgpa >= 3.0) {
-    flavortext = "good";
+    flavortext = ", which is good";
   } else {
-    flavortext = "bad";
+    flavortext = ", which is bad";
   }
 
   function showresult() {
-    console.log(samplestudentgpa);
-    console.log(flavortext);
+    //console.log(samplestudentgpa);
+    //console.log(flavortext);
+    const node = document.createElement("h3");
+    const textnode1 = document.createTextNode(samplestudentgpa);
+    const textnode2 = document.createTextNode(flavortext);
+    node.appendChild(textnode1);
+    node.appendChild(textnode2);
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("result").appendChild(node);
   }
 
   return (
     <div>
       <button onClick={showresult}>Load GPA</button>
+      <div id="result"></div>
     </div>
   );
 };
